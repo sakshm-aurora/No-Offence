14f8934df617b14c00ca7325b1b5834adf3f7f24
Saksham Aurora <49725699+sakshm-aurora@users.noreply.github.com>
2020-11-06 14:10:46 +0530

    Update app.js
    Metamask issue fixed

--- src/js/app.js
+++ src/js/app.js
@@ -9,14 +9,13 @@ App = {
   },
 
   initWeb3: function() {
-    // TODO: refactor conditional
-    if (typeof web3 !== 'undefined') {
+  	 if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
     return App.initContract();
